e094a2f0deca3784bde36e3e949aa037b35ded1d
adityapradhan159 <adityapradhan@tudotechnologies.com>
2022-08-23 14:17:10 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import MessageBox from './Components/MessageBox/MessageBox';
-import Navbar from './Components/Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
       <>
-        <Navbar/>
+      
         <MessageBox/>
       </>
     </div>
